bae28f6d224cd68f84f2e5fa6bc54297ddee5da5
Richard Darst <rkd@zgib.net>
2019-10-11 09:55:53 +0300

    try to separate root variable

--- miniconda.lua
+++ miniconda.lua
@@ -1,12 +1,14 @@
 --prepend_path("PATH", "/home/darstr1/miniconda3-module/bin/")
 
+local root = "/home/darstr1/miniconda3-module/"
+
 if myShellType() == "csh" then
-   execute{cmd="source /home/darstr1/miniconda3-module/bin/activate.csh ''", modeA={"load"}}
+   execute{cmd=table.join{"source ", pathJoin(root, "bin/activate.csh ''"}, modeA={"load"}}
 
 else
    -- The extra '' is needed, or else $@ is set to "module load" and then
    -- "conda activate" gives an error message.
-   execute{cmd="source /home/darstr1/miniconda3-module/bin/activate ''", modeA={"load"}}
+   execute{cmd=table.join{"source ", /home/darstr1/miniconda3-module/bin/activate ''"}, modeA={"load"}}
 
 end
 
