3af0a35e1ba77427cba8d8413c0506197e52296a
Richard Darst <rkd@zgib.net>
2019-10-11 10:14:15 +0300

    work

--- miniconda.lua
+++ miniconda.lua
@@ -1,14 +1,21 @@
+-- module use git/lmod-conda/ ; module load miniconda
+
 --prepend_path("PATH", "/home/darstr1/miniconda3-module/bin/")
 
-local root = "/home/darstr1/miniconda3-module/"
+family("conda")
 
-if myShellType() == "csh" then
-   execute{cmd=table.join{"source ", pathJoin(root, "bin/activate.csh ''"}, modeA={"load"}}
+--local root = "/home/darstr1/miniconda3-module/"
+local root = "~/miniconda3/"
+
+remove_path("PATH", pathJoin(root, "condabin")
+
+if myShellName() == "csh" then
+   execute{cmd=table.concat({"source ", pathJoin(root, "bin/activate.csh ''")}), modeA={"load"}}
 
 else
    -- The extra '' is needed, or else $@ is set to "module load" and then
    -- "conda activate" gives an error message.
-   execute{cmd=table.join{"source ", /home/darstr1/miniconda3-module/bin/activate ''"}, modeA={"load"}}
+   execute{cmd=table.concat({"source ", pathJoin(root, "bin/activate ''")}), modeA={"load"}}
 
 end
 
